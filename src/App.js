a8c8aca45cccc4e2f9fd1eb6688ca8a2d9769faa
Nazarov Dmitry <Difat1@inbox.ru>
2022-06-23 18:35:35 +0300

    work

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Star from "./components/Star";
+
+function App() {
+  return (
+    <div className="App">
+      <Star count={2} />
+    </div>
+  );
+}
+
+export default App;
